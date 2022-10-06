4310b75725feb8032f9076c954dc121946602d7b
Matteo <matteo.lub02@gmail.com>
2022-10-06 09:06:37 +0200

    Cambio numeri codici creati, 10000000

--- src/troll/CreatoreCodici.java
+++ src/troll/CreatoreCodici.java
@@ -8,7 +8,7 @@ public class CreatoreCodici {
 		ArrayList<StringBuilder> codici = new ArrayList<>();
 		Random rand = new Random();
 		
-		for (int i = 0; i < 103; i++) {
+		for (int i = 0; i < 10000000; i++) {
 			StringBuilder s = new StringBuilder();
 			int lunghezza = rand.nextInt(1, 102);
 			for (int j = 0; j < lunghezza; j++) {
